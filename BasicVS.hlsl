992d6ffa7f9cfd84cb7b46775bd708f83ada7b22
YukiMaruichi <k021g1419@g.neec.ac.jp>
2022-04-13 16:43:36 +0900

    02_02

--- BasicVS.hlsl
+++ BasicVS.hlsl
@@ -1,4 +1,5 @@
-float4 main( float4 pos : POSITION ) : SV_POSITION
+float4 main(float4 pos : POSITION) : SV_POSITION
 {
-	return pos;
+	//return pos + float4(1,1,0,0);	//	Xオフセット,Yオフセット
+	return pos * float4(0.5,0.5,1,1);	//	X倍率,Y倍率
 }
