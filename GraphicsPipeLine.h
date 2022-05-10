e0fdc18c305d653d3cc58e5a40209e183c88e72f
YukiMaruichi <k021g1419@g.neec.ac.jp>
2022-05-11 01:16:38 +0900

    blendmord

--- GraphicsPipeLine.h
+++ GraphicsPipeLine.h
@@ -14,9 +14,14 @@ public:
 	D3D12_GRAPHICS_PIPELINE_STATE_DESC desc{};	// グラフィックスパイプライン構造体
 	ID3D12RootSignature* rootSignature;
 	ID3D12PipelineState* state = nullptr;
-
+	D3D12_RENDER_TARGET_BLEND_DESC& blenddesc = desc.BlendState.RenderTarget[0];
 public:
 	GraphicsPipeLine(VertexShader vertShade, D3D12_INPUT_ELEMENT_DESC* inputLayout, UINT size, ID3D12Device* dev);
 
+	void AlphaBlend();
+	void InversBlend();
+	void SubBlend();
+	void AddBlend();
+	void NormalBlend();
 };
 
